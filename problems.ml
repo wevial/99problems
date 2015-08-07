f1e8e1ecd20cec23031334eb01b8fef3d37e6433
Weston Vial <wes@wesvial.com>
2015-08-07 15:12:44 -0400

    first commit

---
+++ problems.ml
@@ -0,0 +1,213 @@
+(* 99 Problems in OCaml
+From: https://ocaml.org/learn/tutorials/99problems.html
+*)
+
+let rec last (ls:'a list):'a option =
+    match ls with
+    | [] -> None
+    | [x] -> Some x
+    | _::ls -> last ls
+
+let rec last_two (ls:'a list):('a * 'a) option =
+    match ls with
+    | [] -> None
+    | [x; y] -> Some (x, y)
+    | _::ls -> last_two ls
+
+let rec at (k:int) (ls:'a list):'a option =
+    match ls with
+    | [] -> None
+    | l::ls -> if k = 1 then Some l else at (k-1) ls
+
+let rec length (ls:'a list):int =
+    match ls with
+    | [] -> 0
+    | _::ls -> 1 + length ls
+
+let rec rev (ls:'a list):'a list =
+    match ls with
+    | [] -> []
+    | l::ls -> (rev ls) @ [l]
+
+let is_palindrome (ls:'a list):bool =
+    rev ls = ls
+
+type 'a node = 
+    | One of 'a
+    | Many of 'a node list
+
+(* REDO flatten *)
+let rec flatten ls (* (ls:'a node list):'a list *) = []
+
+let rec compress ls =
+    match ls with
+    | [] -> []
+    | x::[] -> [x]
+    | x::y::ls -> if x = y
+        then compress (x::ls)
+        else [x] @ compress (y::ls)
+
+(* REDO pack, pack2, encode *)
+
+let encode2 ls = List.map (fun ls -> (List.length ls), (List.hd)) (pack ls)
+
+type 'a rle =
+    | One of 'a
+    | Many of int * 'a
+
+let encodeModified ls =
+    let aux e = 
+        match e with
+        | (1, x) -> One x
+        | (c, x) -> Many (c, x)
+    in List.map aux (encode2 ls)
+
+let decode ls =
+    let rec many count acc x = 
+        if count = 0 then acc else many (count-1) (acc @ [x]) x in
+    let aux e =
+        match e with
+        | One x -> [x]
+        | Many (c, x) -> many c [] x
+    in List.map aux ls
+
+let rec encodeDirect ls =
+    let rec many (count, x) ls = 
+        match ls with
+        | [] -> ((count, x), [])
+        | a::bs ->
+            if a = x then many (count+1, x) bs
+            else ((count, x), [a] @ bs)
+    in
+    match ls with
+    | [] -> []
+    | [x] -> [One x]
+    | x::y::t ->
+        if x = y then 
+            let r = many (2, x) t in 
+            let count = fst (fst r) in
+            [Many (count, x)] @ encodeDirect (snd r)
+        else [One x] @ encodeDirect ([y] @ t)
+
+let rec duplicate ls =
+    match ls with
+    | [] -> []
+    | x::xs -> [x;x] @ duplicate xs
+
+let rec replicate ls n =
+    let rec aux x n acc =
+        if n = 0 then acc else aux x (n-1) ([x] @ acc)
+    in
+    match ls with
+    | [] -> []
+    | x::xs -> (aux x n []) @ replicate xs n
+
+let rec drop ls n =
+    if n <= 0 then ls else
+    match ls with
+    | [] -> []
+    | x::xs ->
+        if n = 1 then xs
+        else [x] @ drop xs (n-1)
+
+let rec split (ls:'a list) (n:int):('a list * 'a list) = 
+    if List.length ls <= n then (ls, []) else
+    match n, ls with
+    | 0, _ -> ([], ls)
+    | _, [] -> ([], [])
+    | 1, [x] -> ([], [x])
+    | n, x::xs -> let r = split xs (n-1) in
+        ([x] @ fst r, snd r)
+
+let slice ls i k =
+    let rec lhs ls i = 
+        match ls, i with
+        | [], _ -> []
+        | ls, 0 -> ls
+        | x::xs, i -> lhs xs (i-1)
+    in
+    let rec rhs ls len =
+        match ls, len with
+        | [], _ | _, 0 -> []
+        | x::xs, len -> [x] @ rhs xs (len-1)
+    in
+    if List.length ls < k then ls else
+    match ls, i, k with 
+    | [], _, _ | _, _, 0 -> []
+    | ls, 0, k -> rhs ls k
+    | ls, i, k -> rhs (lhs ls i) (k-i+1) (* Take indexing at 0 into account *)
+
+let rec rotate ls n = 
+    let ls = if n < 0 then rev ls else ls in
+    match n, ls with
+    | 0, ls -> ls
+    | _, [] -> []
+    | n, x::xs ->
+        if n > 0 then rotate (xs @ [x]) (n-1)
+        else rev (rotate (xs @ [x]) ((abs n)-1))
+
+let rec remove_at i ls =
+    match i, ls with
+    | _, [] -> []
+    | 0, _::xs -> xs
+    | i, x::xs -> [x] @ remove_at (i-1) xs
+
+let rec insert_at ele i ls =
+    match i, ls with
+    | _, [] -> [ele]
+    | 0, ls -> [ele] @ ls
+    | i, x::xs -> [x] @ insert_at ele (i-1) xs
+
+let rec extract_at i ls =
+    match i, ls with
+    | _, [] -> raise Not_found
+    | 0, x::xs -> (x, xs)
+    | i, x::xs -> 
+        let r = extract_at (i-1) xs
+        in (fst r, [x] @ snd r)
+
+let rec range x y =
+    if x = y then [x]
+    else if x < y then [x] @ range (x+1) y
+    else rev (range y x)
+
+(* FROM SOLUTION... - REDO *)
+let rand_select list n =
+    let rec extract acc n = function
+        | [] -> raise Not_found
+        | h::t -> if n = 0 then (h, acc @ t) else extract (h::acc) (n-1) t
+    in
+    let extract_rand list len =
+        extract [] (Random.int len) list
+    in
+    let rec aux n acc list len =
+        if n = 0 then acc else
+            let picked, rest = extract_rand list len in
+            aux (n-1) (picked::acc) rest (len-1)
+    in
+    let len = List.length list in
+    aux (min n len) [] list len
+
+let lotto_select n m = rand_select (range 1 m) n
+
+let permutation ls =
+    let rec aux (acc, ls) len = 
+        if len = 0 then (acc, []) else
+            let r = extract_at (Random.int len) ls in
+            aux (acc @ [fst r], snd r) (len-1)
+    in
+    match ls with
+    | [] -> []
+    | x::xs -> fst (aux ([], ls) (List.length ls))
+
+(* generate combination of K distinct objects chosen from
+ * N elements of a list. *)
+let rec combinations n ls =
+    let rec aux n acc ls c =
+        match n, ls with
+        | 0, _ | _, [] -> []
+        | n, [x] -> aux (n-1) ([x] @ acc) ls [c] (* reduce n by 1 *)
+    in
+    match n, ls with
+    | 0, _ | _, [] -> []
+    | n, x::xs -> aux n [] xs [x]
