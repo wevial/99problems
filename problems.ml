08f8035fe572a355e65f86796a9211439615ff20
Weston Vial <wes@wesvial.com>
2015-09-22 11:01:12 -0400

    Update with latest solutions

--- problems.ml
+++ problems.ml
@@ -36,8 +36,11 @@ type 'a node =
     | One of 'a
     | Many of 'a node list
 
-(* REDO flatten *)
-let rec flatten ls (* (ls:'a node list):'a list *) = []
+let rec flatten ls =
+    match ls with
+    | [] -> []
+    | One x::xs -> [x] @ (flatten xs)
+    | Many x::xs -> (flatten x) @ (flatten xs)
 
 let rec compress ls =
     match ls with
@@ -47,9 +50,18 @@ let rec compress ls =
         then compress (x::ls)
         else [x] @ compress (y::ls)
 
-(* REDO pack, pack2, encode *)
+let pack ls =
+    let rec aux acc ls (curr, c) =
+        match ls with
+        | [] -> []
+        | [x] -> if x = c
+            then acc @ [(curr @ [x])]
+            else acc @ [curr] @ [[x]]
+        | x::xs -> if x = c
+            then aux acc xs (curr @ [x], c)
+            else aux (acc @ [curr]) xs ([x], x)
 
-let encode2 ls = List.map (fun ls -> (List.length ls), (List.hd)) (pack ls)
+let encode ls = List.map (fun ls -> (List.length ls), (List.hd ls)) (pack ls)
 
 type 'a rle =
     | One of 'a
@@ -60,7 +72,7 @@ let encodeModified ls =
         match e with
         | (1, x) -> One x
         | (c, x) -> Many (c, x)
-    in List.map aux (encode2 ls)
+    in List.map aux (encode ls)
 
 let decode ls =
     let rec many count acc x = 
@@ -171,7 +183,7 @@ let rec range x y =
     else if x < y then [x] @ range (x+1) y
     else rev (range y x)
 
-(* FROM SOLUTION... - REDO *)
+(* FROM SOLUTION... *)
 let rand_select list n =
     let rec extract acc n = function
         | [] -> raise Not_found
@@ -202,12 +214,91 @@ let permutation ls =
 
 (* generate combination of K distinct objects chosen from
  * N elements of a list. *)
-let rec combinations n ls =
+let extract k ls =
+    let rec aux k acc emit = function
+        | [] -> acc
+        | hd::tl ->
+            if k = 1 then aux k (emit [h] acc) emit t else
+                let new_emit x = emit (hd::x) in
+                aux k (aux (k-1) acc new_emit t) emit t
+    in
+    let emit x acc = x::acc in
+    aux k [] emit ls
+
+(* let rec combinations n ls =
     let rec aux n acc ls c =
         match n, ls with
         | 0, _ | _, [] -> []
         | n, [x] -> aux (n-1) ([x] @ acc) ls [c] (* reduce n by 1 *)
+        | n, hd::tl -> 
     in
     match n, ls with
     | 0, _ | _, [] -> []
-    | n, x::xs -> aux n [] xs [x]
+    | n, hd::tl -> aux n [] tl [hd] *)
+
+(* let group ls1 ls2 ... *)
+
+let rec length_sort ls =
+    match ls with
+    | [] -> []
+    | hd::tl ->
+        let hdlen = List.length hd in 
+        let smaller = length_sort (List.filter ~f:(fun ls -> List.length ls <= hdlen) tl) in
+        let bigger = length_sort (List.filter ~f:(fun ls -> List.length ls > hdlen) tl) in
+        smaller @ [hd] @ bigger
+
+(* let rec frequency_sort ls = *)
+
+(* Fermat's little theorem *)
+let is_prime n =
+    let k = Int.of_float (3. ** Float.of_int n) mod n
+    in k = 3 mod n
+
+(* Euclidean algorithm *)
+let rec gcd n m =
+    match n, m with
+    | n, 0 -> n
+    | n, m -> gcd m (n mod m)
+
+let coprime n m = 1 = gcd n m
+
+(* Euler's totient function phi(m) *)
+let phi m =
+    if m = 1 then 1
+    else List.length (List.filter ~f:(coprime m) (range 1 m))
+
+let factors n =
+    let rec aux d n =
+        if n = 1 then [] else
+            if n mod d = 0 then d :: aux d (n/d) else aux (d+1) n
+    in
+    aux 2 n
+
+let factors2 n = encode (factors n)
+
+let pow n p = if p < 1 then 1 else Int.of_float ((Float.of_int n) ** (Float.of_int p))
+
+let phi_improved m =
+    let fac = factors2 m in
+    let rec aux fac =
+        match fac with
+        | [] -> 1
+        | (p, m')::tl -> (p-1) * (pow p (m' -1)) * aux tl
+        (* (Int.of_float ((Float.of_int p) ** (Float.of_int (m'-1)))) * aux tl *)
+    in
+    aux m
+
+let timeit f a =
+    let t0 = Unix.gettimeofday() in
+    ignore(f a);
+    let t1 = Unix.gettimeofday() in
+    t1 -. t0
+
+let all_primes n m = List.filter is_prime (range n m)
+
+let goldbach n =
+    let aux d =
+        if is_prime d && is_prime (n-d) then (d, n-d)
+        else aux (d+1)
+    in
+    aux 2
